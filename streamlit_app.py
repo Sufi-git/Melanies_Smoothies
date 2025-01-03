c3c7d23686912c4c78f8f1b2fe862d299fd8b62f
Sufi-git <sufi.git@outlook.com>
2025-01-03 14:42:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ if Ingredients_List:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data.smoothiefroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
