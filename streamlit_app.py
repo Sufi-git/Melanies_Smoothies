552c074f0ad5c8adb8d5977c7dedcfef26112b1e
Sufi-git <sufi.git@outlook.com>
2025-01-03 17:28:27 +0530

    Update streamlit_app.py
    Convert snowpark DF to pandas DF so that we can use LOC function

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,12 @@ st.write("Name on Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# convert snowpark DF to pandas DF so that we can use LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 Ingredients_List = st.multiselect(
