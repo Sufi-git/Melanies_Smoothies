95496db14804b61f707cdbcadc6813606318c3c0
Sufi-git <sufi.git@outlook.com>
2025-01-03 14:39:14 +0530

    Update streamlit_app.py
    added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,5 @@ if Ingredients_List:
 #New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data.smoothiefroot_response.json(), use_container_withd=true)
